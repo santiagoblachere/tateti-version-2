a826dc09d45c0115001a280568247fcf6021250a
Santiago Blachere <blachere.asantiago@gmail.com>
2024-05-04 17:02:22 -0300

    create players+playerSelection

--- javascript.js
+++ javascript.js
@@ -1,41 +1,66 @@
-/* GAMEBOARD */ 
-const body = document.getElementById("body");
-const gbContainer = document.createElement("div")
-gbContainer.classList.add('gameboard')
-body.appendChild(gbContainer)
+/* USER FACTORY */
 
-const gb = (function gameboard() {
+function createPlayer (player) {
+    const selectedPlayer = player;
+    let wins = 0;
+    let loses = 0;
+    const getWinsRecord = () => wins;
+    const addWin = () => wins++;
+    const getLosesRecord = () => loses;
+    const addLoss = () => loses++;
     
-    const gameboard = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    const displayGameboard = () => {
-        gameboard.forEach(element => {
-            const index = gameboard.indexOf(element)
-            const tile = document.createElement("div");
-            tile.innerText = `${index}`
-            tile.classList.add(`${element}`);
-            tile.classList.add("tile")
-            gbContainer.appendChild(tile);
-        });
-   
-    }
-    return { displayGameboard }
     
-})()
 
-gb.displayGameboard()
-
-function createPlayer(order) {
-    if (order === "first") {
-        const player = "X"
-    } else {
-        const player = "O"
-    }
-
-    return { user }
+    return { addWin, selectedPlayer, getWinsRecord, addLoss, getLosesRecord}
 }
+const playerX = createPlayer("x");
+const playerO = createPlayer("o")
+let firstPlayer;
 
 
+const formSection = document.getElementById('formSection')
 
+const form = document.createElement('form');
+formSection.appendChild(form)
+
+const radioX = document.createElement('input');
+radioX.setAttribute('name', 'player');
+radioX.setAttribute('id', 'playerX');
+radioX.setAttribute('type', 'radio');
+const radioXLabel = document.createElement('label');
+radioXLabel.setAttribute('for', 'playerX')
+radioXLabel.innerText = "X"
+form.appendChild(radioXLabel);
+form.appendChild(radioX);
+
+const radioO = document.createElement('input');
+radioO.setAttribute('name', 'player');
+radioO.setAttribute('id', 'playerO');
+radioO.setAttribute('type', 'radio');
+const radioOLabel = document.createElement('label');
+radioOLabel.setAttribute('for', 'playerO')
+radioOLabel.innerText = "O"
+form.appendChild(radioOLabel);
+form.appendChild(radioO);
+
+const submit = document.createElement('input');
+submit.setAttribute('type', 'submit');
+submit.setAttribute('value', 'PLAY')
+form.appendChild(submit);
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+    if (radioX.checked) {
+        firstPlayer = playerX;
+        
+    } else if (radioO.checked) {
+        firstPlayer = playerO;
+    } else {
+        alert("Pick a symbol!")
+        return
+    }
+    
+}
 
 
 
