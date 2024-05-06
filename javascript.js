c758d962481cd6a6585a76943a3ea901f633f059
Santiago Blachere <blachere.asantiago@gmail.com>
2024-05-06 17:26:56 -0300

    create gameplay logic

--- javascript.js
+++ javascript.js
@@ -18,6 +18,7 @@ const playerX = createPlayer("x");
 const playerO = createPlayer("o")
 let firstPlayer;
 let secondPlayer;
+NodeList.prototype.indexOf = Array.prototype.indexOf;
 
 const gameboardSection = document.getElementById('gameboardSection')
 const formSection = document.getElementById('formSection')
@@ -52,6 +53,7 @@ form.appendChild(submit);
 
 form.onsubmit = (e) => {
     e.preventDefault()
+    console.log(radioX.checked)
     if (radioX.checked) {
         firstPlayer = playerX.selectedPlayer;
         secondPlayer = playerO.selectedPlayer;
@@ -67,7 +69,7 @@ form.onsubmit = (e) => {
 }
 
 function createGameboard() {
-    let turn = 1;
+    let turn = 0;
     let tilesArray = [1,2,3,4,5,6,7,8,9]
     
     const changeTurn = () => { turn++ }
@@ -78,29 +80,73 @@ function createGameboard() {
             tile.classList.add('tile')
             tile.innerText = el
             gameboardSection.appendChild(tile)
+            tile.addEventListener('click', () => {
+                let turn = gb.getTurn()
+                
+                if (turn% 2 === 0) {
+                    tile.innerText = `${firstPlayer}`;
+                    
+                    gb.changeTurn()
+                } else {
+                    tile.innerText = `${secondPlayer}`;
+                    gb.changeTurn()
+                }
+                checkConditions(tiles)
+                /* falta que con cada click chequee win/lose/tie conditions y que se auto deinhabilite el event listener del tile */
+            })
         })
+    }
+    const resetGb = (tiles) => {
+        tiles.forEach( (tile) => {
+            tile.innerText = ""
+        })
+    }
+    const checkConditions = (tiles) => {
+        if ( (tiles[0].innerText === "x" && tiles[1].innerText === "x" && tiles[2].innerText === "x" ) ||
+             (tiles[3].innerText === "x" && tiles[4].innerText === "x" && tiles[5].innerText === "x") || 
+             (tiles[6].innerText === "x" && tiles[7].innerText === "x" && tiles[8].innerText === "x") || 
+             (tiles[0].innerText === "x" && tiles[3].innerText === "x" && tiles[6].innerText === "x") || 
+             (tiles[1].innerText === "x" && tiles[4].innerText === "x" && tiles[7].innerText === "x") || 
+             (tiles[2].innerText === "x" && tiles[5].innerText === "x" && tiles[8].innerText === "x") || 
+             (tiles[0].innerText === "x" && tiles[4].innerText === "x" && tiles[8].innerText === "x") || 
+             (tiles[2].innerText === "x" && tiles[3].innerText === "x" && tiles[5].innerText === "x")
+        )
+        {
+            playerX.addWin()
+            playerO.addLoss()
+            resetGb(tiles);
+            console.log(playerX.getWinsRecord())
+            console.log(playerO.getLosesRecord())
+        }
+        else if ( (tiles[0].innerText === "o" && tiles[1].innerText === "o" && tiles[2].innerText === "o") ||
+        (tiles[3].innerText === "o" && tiles[4].innerText === "o" && tiles[5].innerText === "o") || 
+        (tiles[6].innerText === "o" && tiles[7].innerText === "o" && tiles[8].innerText === "o") || 
+        (tiles[0].innerText === "o" && tiles[3].innerText === "o" && tiles[6].innerText === "o") || 
+        (tiles[1].innerText === "o" && tiles[4].innerText === "o" && tiles[7].innerText === "o") || 
+        (tiles[2].innerText === "o" && tiles[5].innerText === "o" && tiles[8].innerText === "o") || 
+        (tiles[0].innerText === "o" && tiles[4].innerText === "o" && tiles[8].innerText === "o") || 
+        (tiles[2].innerText === "o" && tiles[3].innerText === "o" && tiles[5].innerText === "o")
+        
+   ){
+            playerO.addWin()
+            playerX.addLoss()
+            resetGb(tiles);
+            console.log(playerO.getWinsRecord())
+            console.log(playerX.getLosesRecord())
+        }
+
     }
 
 
-    return { changeTurn, paintGb, getTurn }
+    return { changeTurn, paintGb, getTurn, tilesArray, checkConditions }
 }
 const gb = createGameboard()
 gb.paintGb()
 const tiles = document.querySelectorAll('.tile')
-tiles.forEach( (tile) => {
-    tile.addEventListener('click', () => {
-        let turn = gb.getTurn()
-        console.log(turn)
-        if (turn% 2 === 0) {
-            tile.innerText = `${firstPlayer}`;
-            gb.changeTurn()
-        } else {
-            tile.innerText = `${secondPlayer}`;
-            gb.changeTurn()
-        }
-        /* falta que con cada click chequee win/lose/tie conditions y que se auto deinhabilite el event listener del tile */
-    }) 
-})
+
+
+
+
 
 
 
